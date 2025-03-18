915c079fe56013adc46f8c3eaf753f34d687a0c5
Anderson Sinche <andersonsinche@MacBook-Pro.local>
2025-03-18 09:20:16 -0500

    [feat] get results

--- src/config/cluster.ts
+++ src/config/cluster.ts
@@ -1,15 +1,25 @@
 import { Cluster } from 'puppeteer-cluster';
 
 export let cluster: Cluster; // Definimos `cluster` fuera de la función para poder exportarlo
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+puppeteer.use(StealthPlugin());
 
 export const initCluster = async () => {
     cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT,
         maxConcurrency: 5,
+        puppeteer,
+        timeout: 120000,
         puppeteerOptions: {
             headless: false,
-            args: ['--no-sandbox', '--disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure','--disable-setuid-sandbox','--start-maximized'],
-            timeout: 300000,
+            args: ['--no-sandbox',
+                '--start-maximized',
+                '--disable-web-security',
+                '--disable-features=IsolateOrigins,site-per-process'
+              ],
+            timeout: 90000,
             defaultViewport: null
         },
     });
--- src/services/wong.service.ts
+++ src/services/wong.service.ts
@@ -1,18 +1,36 @@
 import { cluster } from '../config/cluster';
-
+import fs from 'fs';
 export class WongService {
     static async getItemsbyURL(url: string): Promise<string> {
         return await cluster.execute(url, async ({ page, data }) => {
+            await page.setUserAgent(
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+              );
             
             console.log('cargo')
             await page.goto(data, {
                 waitUntil: 'domcontentloaded',
-                // Remove the timeout
-                timeout: 0
+                timeout: 90000
             });
             console.log('cargo')
-            
-            return await page.screenshot();
+            console.log("🔄 Haciendo scroll para cargar productos...");
+            await page.evaluate(() => {
+                window.scrollBy(0, window.innerHeight);
+            });
+            await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 5000)));
+            const html = await page.content();
+            console.log(html);
+            fs.writeFileSync('pagina.html', html);
+
+            console.log("✅ HTML guardado en 'pagina.html'");
+            const info = await page.evaluate(() => {
+                const products = [...document.querySelectorAll('.pod-details')].map(el=>{
+                    return (el as HTMLElement)?.innerText
+                })
+                return products
+            })
+            console.log(info)
+            return await page.screenshot()
         });
     }
 }
